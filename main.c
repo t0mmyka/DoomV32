6fafdc593ac4a7ee82ac16cbdc6b9633288a7c45
t0mmyka <tkastne1@corning-cc.edu>
2025-06-21 01:35:29 -0400

    bsp rendering testing with more complex scene

--- main.c
+++ main.c
@@ -71,8 +71,8 @@ struct BspBranch
     float      HyperDx;
     bool       BranchSide;
     BspBranch* parentNode;
-    BspBranch* leftNode;
     BspBranch* rightNode;
+    BspBranch* leftNode;
     BspLeaf*   leaf;
 };
 
@@ -670,8 +670,11 @@ void bspRender(
 
 void main(void)
 {
+    int         TIME;
     Player      user;
     Texture     emptyTexture;
+    FrameBuffer drawClip;
+    FrameBuffer cleanBuffer;
     Sector      Room0;
     Sector      Room1;
     Segment     wall0;
@@ -680,18 +683,26 @@ void main(void)
     Segment     wall3;
     Segment     wall4;
     Segment     wall5;
-    FrameBuffer drawClip;
-    FrameBuffer cleanBuffer;
+    Segment     wall6;
+    Segment     wall7;
+    Segment     wall8;
+    Segment     wall9;
+    Segment     wall10;
     BspLeaf     leaf0;
     BspLeaf     leaf1;
     BspLeaf     leaf2;
+    BspLeaf     leaf3;
     BspBranch   rootNode;
     BspBranch   node0;
     BspBranch   node1;
+    BspBranch   node2;
+    BspBranch   node3;
+    BspBranch   node4;
+    BspBranch   node5;
 
-    user.xPos = 0.00;
-    user.yPos = 0.00;
-    user.zPos = 2.00;
+    user.xPos =      0.00;
+    user.yPos =      0.00;
+    user.zPos =     10.00;
     user.direction = 0.0;
     user.dirSin = sin(user.direction);
     user.dirCos = cos(user.direction);
@@ -701,92 +712,6 @@ void main(void)
     emptyTexture.yCord  = 0;
     emptyTexture.width  = 32;
     emptyTexture.height = 32;
-
-    Room0.floorHeight   =  0.0;
-    Room0.ceilingHeight =  30.0;
-    Room0.roomHeight    = Room0.ceilingHeight - Room0.floorHeight;
-    Room0.floor         = &emptyTexture;
-    Room0.ceiling       = &emptyTexture;
-
-    Room1 = Room0;
-
-    wall0.xPos        =  16.0;
-    wall0.yPos        =  48.0;
-    wall0.dx          =   0.0;
-    wall0.dy          = -64.0;
-    wall0.length      = sqrt(wall0.dx * wall0.dx + wall0.dy * wall0.dy);
-    wall0.sectorRight  = &Room0;
-    wall0.sectorRight = &Room1;
-    wall0.bottom      = &emptyTexture;
-    wall0.middle      = &emptyTexture;
-    wall0.top         = &emptyTexture;
-    wall0.yOffset     =   0.0;
-    wall0.xOffset     =   0.0;
-
-    wall1.xPos        =  16.0;
-    wall1.yPos        = -16.0;
-    wall1.dx          = -32.0;
-    wall1.dy          =   0.0;
-    wall1.length      = sqrt(wall1.dx * wall1.dx + wall1.dy * wall1.dy);
-    wall1.sectorRight = &Room0;
-    wall1.sectorLeft  = &Room1;
-    wall1.bottom      = &emptyTexture;
-    wall1.middle      = &emptyTexture;
-    wall1.top         = &emptyTexture;
-    wall1.yOffset     =  0.0;
-    wall1.xOffset     =  0.0;
-
-    wall2.xPos        = -16.0;
-    wall2.yPos        = -16.0;
-    wall2.dx          =   0.0;
-    wall2.dy          =  32.0;
-    wall2.length      = sqrt(wall2.dx * wall2.dx + wall2.dy * wall2.dy);
-    wall2.sectorRight = &Room0;
-    wall2.sectorLeft  = &Room1;
-    wall2.bottom      = &emptyTexture;
-    wall2.middle      = &emptyTexture;
-    wall2.top         = &emptyTexture;
-    wall2.yOffset     =  0.0;
-    wall2.xOffset     =  0.0;
-
-    wall3.xPos        = -16.0;
-    wall3.yPos        =  16.0;
-    wall3.dx          = -32.0;
-    wall3.dy          =   0.0;
-    wall3.length      = sqrt(wall3.dx * wall3.dx + wall3.dy * wall3.dy);
-    wall3.sectorRight = &Room0;
-    wall3.sectorLeft  = &Room1;
-    wall3.bottom      = &emptyTexture;
-    wall3.middle      = &emptyTexture;
-    wall3.top         = &emptyTexture;
-    wall3.yOffset     =  0.0;
-    wall3.xOffset     =  0.0;
-
-    wall4.xPos        = -48.0;
-    wall4.yPos        =  16.0;
-    wall4.dx          =   0.0;
-    wall4.dy          =  32.0;
-    wall4.length      = sqrt(wall4.dx * wall4.dx + wall4.dy * wall4.dy);
-    wall4.sectorRight = &Room0;
-    wall4.sectorLeft  = &Room1;
-    wall4.bottom      = &emptyTexture;
-    wall4.middle      = &emptyTexture;
-    wall4.top         = &emptyTexture;
-    wall4.yOffset     =  0.0;
-    wall4.xOffset     =  0.0;
-
-    wall5.xPos        = -48.0;
-    wall5.yPos        =  48.0;
-    wall5.dx          =  64.0;
-    wall5.dy          =   0.0;
-    wall5.length      = sqrt(wall5.dx * wall5.dx + wall5.dy * wall5.dy);
-    wall5.sectorRight = &Room0;
-    wall5.sectorLeft  = &Room1;
-    wall5.bottom      = &emptyTexture;
-    wall5.middle      = &emptyTexture;
-    wall5.top         = &emptyTexture;
-    wall5.yOffset     =  0.0;
-    wall5.xOffset     =  0.0;
 
     for(int i = 0; i < SCREENWIDTH; i++)
     {
@@ -799,44 +724,238 @@ void main(void)
     }
     cleanBuffer = drawClip;
 
-    Segment*[4] leaf0List = {&wall0, &wall1, &wall2, NULL};
+    Room0.floorHeight   =  0.0;
+    Room0.ceilingHeight = 32.0;
+    Room0.roomHeight    = Room0.ceilingHeight - Room0.floorHeight;
+    Room0.floor         = &emptyTexture;
+    Room0.ceiling       = &emptyTexture;
+
+    Room1 = Room0;
+
+    wall0.xPos        =   48.0;
+    wall0.yPos        =   96.0;
+    wall0.dx          =   80.0;
+    wall0.dy          =    0.0;
+    wall0.length      = sqrt(wall0.dx * wall0.dx + wall0.dy * wall0.dy);
+    wall0.sectorRight = &Room0;
+    wall0.sectorRight = &Room1;
+    wall0.bottom      = &emptyTexture;
+    wall0.middle      = &emptyTexture;
+    wall0.top         = &emptyTexture;
+    wall0.yOffset     =    0.0;
+    wall0.xOffset     =   48.0;
+
+    wall1.xPos        =  128.0;
+    wall1.yPos        =   96.0;
+    wall1.dx          =    0.0;
+    wall1.dy          =  -96.0;
+    wall1.length      = sqrt(wall1.dx * wall1.dx + wall1.dy * wall1.dy);
+    wall1.sectorRight = &Room0;
+    wall1.sectorRight = &Room1;
+    wall1.bottom      = &emptyTexture;
+    wall1.middle      = &emptyTexture;
+    wall1.top         = &emptyTexture;
+    wall1.yOffset     =    0.0;
+    wall1.xOffset     =    0.0;
+
+    wall2.xPos        =  128.0;
+    wall2.yPos        =    0.0;
+    wall2.dx          =   -8.0;
+    wall2.dy          =    0.0;
+    wall2.length      = sqrt(wall2.dx * wall2.dx + wall2.dy * wall2.dy);
+    wall2.sectorRight = &Room0;
+    wall2.sectorRight = &Room1;
+    wall2.bottom      = &emptyTexture;
+    wall2.middle      = &emptyTexture;
+    wall2.top         = &emptyTexture;
+    wall2.yOffset     =    0.0;
+    wall2.xOffset     =    0.0;
+
+    wall3.xPos        =   76.8;
+    wall3.yPos        =   57.6;
+    wall3.dx          =  -19.2;
+    wall3.dy          =   25.6;
+    wall3.length      = sqrt(wall3.dx * wall3.dx + wall3.dy * wall3.dy);
+    wall3.sectorRight = &Room0;
+    wall3.sectorRight = &Room1;
+    wall3.bottom      = &emptyTexture;
+    wall3.middle      = &emptyTexture;
+    wall3.top         = &emptyTexture;
+    wall3.yOffset     =    0.0;
+    wall3.xOffset     =    0.0;
+
+    wall4.xPos        =  120.0;
+    wall4.yPos        =    0.0;
+    wall4.dx          = -120.0;
+    wall4.dy          =    0.0;
+    wall4.length      = sqrt(wall4.dx * wall4.dx + wall4.dy * wall4.dy);
+    wall4.sectorRight = &Room0;
+    wall4.sectorRight = &Room1;
+    wall4.bottom      = &emptyTexture;
+    wall4.middle      = &emptyTexture;
+    wall4.top         = &emptyTexture;
+    wall4.yOffset     =    0.0;
+    wall4.xOffset     =    8.0;
+
+    wall5.xPos        =   51.2;
+    wall5.yPos        =   38.4;
+    wall5.dx          =   25.6;
+    wall5.dy          =   19.2;
+    wall5.length      = sqrt(wall5.dx * wall5.dx + wall5.dy * wall5.dy);
+    wall5.sectorRight = &Room0;
+    wall5.sectorRight = &Room1;
+    wall5.bottom      = &emptyTexture;
+    wall5.middle      = &emptyTexture;
+    wall5.top         = &emptyTexture;
+    wall5.yOffset     =    0.0;
+    wall5.xOffset     =    0.0;
+
+    wall6.xPos        =    0.0;
+    wall6.yPos        =    0.0;
+    wall6.dx          =    0.0;
+    wall6.dy          =   96.0;
+    wall6.length      = sqrt(wall6.dx * wall6.dx + wall6.dy * wall6.dy);
+    wall6.sectorRight = &Room0;
+    wall6.sectorRight = &Room1;
+    wall6.bottom      = &emptyTexture;
+    wall6.middle      = &emptyTexture;
+    wall6.top         = &emptyTexture;
+    wall6.yOffset     =    0.0;
+    wall6.xOffset     =    0.0;
+
+    wall7.xPos        =    0.0;
+    wall7.yPos        =   96.0;
+    wall7.dx          =    8.0;
+    wall7.dy          =    0.0;
+    wall7.length      = sqrt(wall7.dx * wall7.dx + wall7.dy * wall7.dy);
+    wall7.sectorRight = &Room0;
+    wall7.sectorRight = &Room1;
+    wall7.bottom      = &emptyTexture;
+    wall7.middle      = &emptyTexture;
+    wall7.top         = &emptyTexture;
+    wall7.yOffset     =    0.0;
+    wall7.xOffset     =    0.0;
+
+    wall8.xPos        =   32.0;
+    wall8.yPos        =   64.0;
+    wall8.dx          =   19.2;
+    wall8.dy          =  -25.6;
+    wall8.length      = sqrt(wall8.dx * wall8.dx + wall8.dy * wall8.dy);
+    wall8.sectorRight = &Room0;
+    wall8.sectorRight = &Room1;
+    wall8.bottom      = &emptyTexture;
+    wall8.middle      = &emptyTexture;
+    wall8.top         = &emptyTexture;
+    wall8.yOffset     =    0.0;
+    wall8.xOffset     =    0.0;
+
+    wall9.xPos        =    8.0;
+    wall9.yPos        =   96.0;
+    wall9.dx          =   40.2;
+    wall9.dy          =    0.0;
+    wall9.length      = sqrt(wall9.dx * wall9.dx + wall9.dy * wall9.dy);
+    wall9.sectorRight = &Room0;
+    wall9.sectorRight = &Room1;
+    wall9.bottom      = &emptyTexture;
+    wall9.middle      = &emptyTexture;
+    wall9.top         = &emptyTexture;
+    wall9.yOffset     =    0.0;
+    wall9.xOffset     =    8.0;
+
+    wall10.xPos        =   57.6;
+    wall10.yPos        =   83.2;
+    wall10.dx          =  -25.6;
+    wall10.dy          =  -19.2;
+    wall10.length      = sqrt(wall10.dx * wall10.dx + wall10.dy * wall10.dy);
+    wall10.sectorRight = &Room0;
+    wall10.sectorRight = &Room1;
+    wall10.bottom      = &emptyTexture;
+    wall10.middle      = &emptyTexture;
+    wall10.top         = &emptyTexture;
+    wall10.yOffset     =    0.0;
+    wall10.xOffset     =    0.0;
+
+    Segment*[5] leaf0List = {&wall0, &wall1, &wall2, &wall3, NULL};
     leaf0.segList = &(leaf0List[0]);
 
-    Segment*[4] leaf1List = {&wall3, &wall4, &wall5, NULL};
+    Segment*[3] leaf1List = {&wall4, &wall5, NULL};
     leaf1.segList = &(leaf1List[0]);
 
-    Segment*[1] leaf2List = {NULL};
+    Segment*[4] leaf2List = {&wall6, &wall7, &wall8, NULL};
     leaf2.segList = &(leaf2List[0]);
 
-    rootNode.HyperX     =  16.0;
-    rootNode.HyperY     =  48.0;
-    rootNode.HyperDx    = -32.0;
-    rootNode.HyperDy    = -32.0;
-    rootNode.BranchSide = RIGHT;
-    rootNode.leftNode   = &node0;
-    rootNode.rightNode  = &node1;
+    Segment*[3] leaf3List = {&wall9, &wall10, NULL};
+    leaf3.segList = &(leaf3List[0]);
+
+    rootNode.HyperX     =   76.8;
+    rootNode.HyperY     =   57.6;
+    rootNode.HyperDx    =  -19.2;
+    rootNode.HyperDy    =   25.6;
+    rootNode.BranchSide = NULL;
+    rootNode.rightNode  = &node0;
+    rootNode.leftNode   = &node1;
     rootNode.parentNode = NULL;
     rootNode.leaf       = NULL;
 
-    node0.HyperX     =   0.0;
-    node0.HyperY     =   0.0;
-    node0.HyperDx    =   0.0;
-    node0.HyperDy    =   0.0;
+    node0.HyperX     = NULL;
+    node0.HyperY     = NULL;
+    node0.HyperDx    = NULL;
+    node0.HyperDy    = NULL;
     node0.BranchSide = RIGHT;
-    node0.leftNode   = NULL;
     node0.rightNode  = NULL;
+    node0.leftNode   = NULL;
     node0.parentNode = &rootNode;
     node0.leaf       = &leaf0;
 
-    node1.HyperX     =  0.0;
-    node1.HyperY     =  0.0;
-    node1.HyperDx    =  0.0;
-    node1.HyperDy    =  0.0;
+    node1.HyperX     =   51.2;
+    node1.HyperY     =   38.4;
+    node1.HyperDx    =   25.6;
+    node1.HyperDy    =   19.2;
     node1.BranchSide = LEFT;
-    node1.leftNode   = NULL;
-    node1.rightNode  = NULL;
+    node1.rightNode  = &node2;
+    node1.leftNode   = &node3;
     node1.parentNode = &rootNode;
-    node1.leaf       = &leaf1;
+    node1.leaf       = NULL;
+
+    node2.HyperX     = NULL;
+    node2.HyperY     = NULL;
+    node2.HyperDx    = NULL;
+    node2.HyperDy    = NULL;
+    node2.BranchSide = RIGHT;
+    node2.rightNode  = NULL;
+    node2.leftNode   = NULL;
+    node2.parentNode = &rootNode;
+    node2.leaf       = &leaf1;
+
+    node3.HyperX     =   32.0;
+    node3.HyperY     =   64.0;
+    node3.HyperDx    =   19.2;
+    node3.HyperDy    =  -25.6;
+    node3.BranchSide = LEFT;
+    node3.rightNode  = &node4;
+    node3.leftNode   = &node5;
+    node3.parentNode = &rootNode;
+    node3.leaf       = NULL;
+
+    node4.HyperX     = NULL;
+    node4.HyperY     = NULL;
+    node4.HyperDx    = NULL;
+    node4.HyperDy    = NULL;
+    node4.BranchSide = RIGHT;
+    node4.rightNode  = NULL;
+    node4.leftNode   = NULL;
+    node4.parentNode = &rootNode;
+    node4.leaf       = &leaf2;
+
+    node5.HyperX     = NULL;
+    node5.HyperY     = NULL;
+    node5.HyperDx    = NULL;
+    node5.HyperDy    = NULL;
+    node5.BranchSide = LEFT;
+    node5.rightNode  = NULL;
+    node5.leftNode   = NULL;
+    node5.parentNode = &rootNode;
+    node5.leaf       = &leaf3;
 
     float speedX;
     float speedY;
@@ -859,6 +978,7 @@ void main(void)
         //drawBspLeaf(&drawClip, &leaf0, &user);
         //drawBspLeaf(&drawClip, &leaf1, &user);
         //drawBspLeaf(&drawClip, &leaf2, &user);
+        //drawBspLeaf(&drawClip, &leaf3, &user);
 
         if(gamepad_button_b() > 0)
             user.direction += 0.001 * gamepad_button_b();
@@ -882,9 +1002,11 @@ void main(void)
 
         user.xPos += user.dirCos * speedX - user.dirSin * speedY;
         user.yPos += user.dirCos * speedY + user.dirSin * speedX;
+        user.zPos =  10.0 + cos((float)TIME / 30.0);
 
         drawClip = cleanBuffer;
 
+        TIME++;
         end_frame();
     }
 
